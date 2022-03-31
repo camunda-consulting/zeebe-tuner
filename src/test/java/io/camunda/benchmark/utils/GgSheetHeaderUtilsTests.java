50d15108430d87f6ee4444f2968e0387d951d0f1
Christophe Dame <christophe.dame@camunda.com>
2022-03-31 17:20:21 +0200

    add some more inputs into zeebe-value file

--- src/test/java/io/camunda/benchmark/utils/GgSheetHeaderUtilsTests.java
+++ src/test/java/io/camunda/benchmark/utils/GgSheetHeaderUtilsTests.java
@@ -9,5 +9,9 @@ public class GgSheetHeaderUtilsTests {
 	void toCamelCaseTest() {
 		String result = GgSheetHeaderUtils.toCamelCase("Max payload size (KiB)");
 		assertThat(result).isEqualTo("maxPayloadSize");
+		result = GgSheetHeaderUtils.toCamelCase("CPU Thread Pool Size/Node");
+		assertThat(result).isEqualTo("cpuThreadPoolSizeNode");
+		result = GgSheetHeaderUtils.toCamelCase("vCPUs (Hyperthreads/node)");
+		assertThat(result).isEqualTo("vcpus");
 	}
 }
