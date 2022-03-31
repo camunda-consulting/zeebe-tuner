d7f86b8ca0b194bf1981c70a3141c5c7e24a73cb
Falko Menge <falko.menge@camunda.com>
2022-03-31 18:12:36 +0200

    Add shell runner

---
+++ runner/download-config-and-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# retrieve machine types from K8s
+machineType=$(kubectl get node -o json | jq -r '.items[0].metadata.labels."beta.kubernetes.io/instance-type"')
+echo "Machine type: ${machineType}"
+
+# download configs from Google Sheet
+java -DmachineType=$machineType -jar sheets-exporter.jar
+
+# and run all tests
+./run-all-tests.sh
