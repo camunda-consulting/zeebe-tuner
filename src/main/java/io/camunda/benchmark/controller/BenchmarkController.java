5913a894869f996702f8cbd18287118bfd30b9c0
Christophe Dame <christophe.dame@camunda.com>
2022-04-01 10:57:47 +0200

    Include a single runner per scenario and expose some APIs

--- src/main/java/io/camunda/benchmark/controller/BenchmarkController.java
+++ src/main/java/io/camunda/benchmark/controller/BenchmarkController.java
@@ -1,14 +1,20 @@
 package io.camunda.benchmark.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,10 +40,45 @@ public class BenchmarkController {
 	@Autowired
 	private ZeebeStarterService zeebeStarterService;
 	
+	private Map<String, Integer> scenariiIdxMap;
+	
 	@GetMapping(value = "/tearDown")
 	public void tearDown() throws IOException, InterruptedException {
 		eksService.cleanEksCluster();
 	}
+	
+	@GetMapping(value = "/scenarii")
+	public Set<String> scenarii() {
+		if (scenariiIdxMap!=null) {
+			return scenariiIdxMap.keySet();
+		}
+		scenariiIdxMap = new HashMap<>();
+		for(int i=0;i<scenarioBuilderService.getNbScenarii();i++) {
+			scenariiIdxMap.put(scenarioBuilderService.getInputs(i).get("testCaseName"), i);
+		}
+		return scenariiIdxMap.keySet();
+	}
+	
+	@PostMapping(value = "/execute/{scenario}")
+	public String scenarii(@PathVariable String scenario) throws IOException, InterruptedException {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+
+		processBuilder.command("bash", "-c", "./testruns/"+scenario+"/run.sh");
+		Process process = processBuilder.start();
+		StringBuilder output = new StringBuilder();
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(process.getInputStream()));
+
+		String line;
+		while ((line = reader.readLine()) != null) {
+			output.append(line + "\n");
+		}
+		return output.toString();
+		//Map<String, Object> result =ProcessUtils.execBlocking("./testruns/"+scenario+"/run.sh");
+		//return (String) result.get("output");
+	}
+		
 	
 	@PostConstruct
     private void run() throws IOException, GeneralSecurityException, InterruptedException {
