d5dc9b12e098da85b193b42dfc56e88469af967f
Falko Menge <falko.menge@camunda.com>
2022-04-01 14:42:02 +0200

    Update configs

--- src/main/resources/benchmarktemplates/run.sh
+++ src/main/resources/benchmarktemplates/run.sh
@@ -1,18 +1,6 @@
 echo "###############################################"
 echo "Running test for config: ${testCaseName}"
 #
-namespace="${namespace}"
-#
-kubectl create namespace $namespace
-kubens $namespace
-#
 # Start Zeebe
 echo 'Starting Zeebe...'
-make zeebe
-#
-echo 'Waiting for the Zeebe pods to be running...'
-while [ "$(kubectl get statefulset ${namespace}-zeebe -o json -n ${namespace} | jq -r '.status.readyReplicas == .status.replicas')" != "true" ]; do sleep 1; printf '.'; done
-#
-make camunda-cloud-benchmark
-#
-make url-grafana
+make
