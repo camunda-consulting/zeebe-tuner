b7994fbf974c2c4de2f3416553988b27639c3bd4
Falko Menge <falko.menge@camunda.com>
2022-06-28 16:20:08 +0200

    Make it work

--- runner/run-all-tests.sh
+++ runner/run-all-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir testruns/done
+mkdir testruns-done
 for filename in testruns/*/;
 do
 #    testScenarioName=$(basename "$filename")
@@ -7,5 +7,5 @@ do
 #	  mv $filename "testruns/$testScenarioName.yaml"
 	  ./run-single-test.sh "$filename"
 #    mv "testruns/$testScenarioName.yaml" testruns/done/
-#    mv "testruns/$testScenarioName" testruns/done/
+    mv "$filename" testruns-done/
 done
--- runner/run-single-test.sh
+++ runner/run-single-test.sh
@@ -9,29 +9,14 @@ cd ${testScenarioName}
 
 # Start Zeebe
 echo 'Starting Zeebe...'
-#make zeebe await-zeebe
 make
-
-#make port-zeebe &
-#sleep 3
-#make bpmn
-#kill [1]
-
-echo 'Waiting for the Zeebe pods to be running...'
-while [ "$(kubectl get statefulset ${testScenarioName}-zeebe -o json -n ${namespace} | jq -r '.status.readyReplicas == .status.replicas')" != "true" ]; do sleep 1; printf '.'; done
-#while [ "$(kubectl get statefulset zeebe -o go-template --template='{{if eq .status.readyReplicas .status.replicas}}true{{end}}')" != "true" ]; do sleep 1; printf '.'; done
-# produced: error: error executing template "{{if eq .status.readyReplicas .status.replicas}}true{{end}}": template: output:1:5: executing "output" at <eq .status.readyReplicas .status.replicas>: error calling eq: invalid type for comparison
-
-# Start Starter
-#echo 'Starting Starter and Worker...'
-#make starter worker
 
 ## wait for the starter job to be finished
 echo 'Waiting for completion of benchmark run...'
-kubectl wait --for=condition=complete job/starter --timeout=1200s
+#kubectl wait --for=condition=complete job/starter --timeout=1200s
+sleep 20m
 
 #############
 ## Cleanup
 echo 'Finished. Cleaning up now...'
 make clean
-kubectl delete namespace $namespace
