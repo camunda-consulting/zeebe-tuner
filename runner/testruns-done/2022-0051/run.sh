a6a48b9481a1f396d9df34298156dcb8ad251d04
Falko Menge <falko.menge@camunda.com>
2022-06-28 16:20:27 +0200

    Add executed runs

---
+++ runner/testruns-done/2022-0051/run.sh
@@ -0,0 +1,6 @@
+echo "###############################################"
+echo "Running test for config: 2022-0051"
+#
+# Start Zeebe
+echo 'Starting Zeebe...'
+make
---
+++ runner/testruns-done/2202-0052/run.sh
@@ -0,0 +1,6 @@
+echo "###############################################"
+echo "Running test for config: 2202-0052"
+#
+# Start Zeebe
+echo 'Starting Zeebe...'
+make
