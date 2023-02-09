a9c071ab75cdfbffd9a6dba776d7744dd8127877
Falko Menge <falko.menge@camunda.com>
2023-02-09 14:37:05 +0100

    Add TTL, exporter 0.0.7, generate sleep script

--- src/main/resources/benchmarktemplates/run.sh
+++ src/main/resources/benchmarktemplates/run.sh
@@ -1,6 +1,9 @@
-echo "###############################################"
-echo "Running test for config: ${testCaseName}"
-#
+#echo "###############################################"
+#echo "Running test for config: ${testCaseName}"
+
+echo "Waiting ${loadGeneratorStarter.runDuration} seconds for completion of benchmark run ${testCaseName} ..."
+sleep ${loadGeneratorStarter.runDuration}
+
 # Start Zeebe
-echo 'Starting Zeebe...'
-make
+#echo 'Starting Zeebe...'
+#make
