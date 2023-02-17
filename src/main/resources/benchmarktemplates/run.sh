a9c3f82e43bc46c44e0f53413f7132e51edc3996
Falko Menge <falko.menge@camunda.com>
2023-02-17 15:06:28 +0100

    Record Helm chart version

--- src/main/resources/benchmarktemplates/run.sh
+++ src/main/resources/benchmarktemplates/run.sh
@@ -1,6 +1,7 @@
 #echo "###############################################"
 #echo "Running test for config: ${testCaseName}"
 
+helm search repo camunda/camunda-platform --output yaml > helm-chart-version.yaml
 
 echo "Waiting $((${loadGeneratorStarter.runDuration}/60)) minutes for completion of benchmark run ${testCaseName} ..."
 sleep ${loadGeneratorStarter.runDuration}
