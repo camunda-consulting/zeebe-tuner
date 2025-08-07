f1b7587f655ff330cc4a80dc95b18f43910f4d91
Falko Menge <falko.menge@camunda.com>
2025-08-07 02:03:45 +0200

    Add separate ES for Optimize; improve config & install

---
+++ src/main/resources/benchmarktemplates/elasticsearch-optimize.mk
@@ -0,0 +1,23 @@
+# Camunda components will be installed into the following Kubernetes namespace
+namespace ?= camunda
+# Helm release name
+release ?= optimize
+# Helm chart coordinates for Camunda
+chart ?= camunda/camunda-platform
+
+chartValues ?= elasticsearch-optimize.yaml --version=${engine.helmChartVersion}
+
+.PHONY: all
+all: install-camunda pods await-elasticsearch
+
+.PHONY: clean
+clean: uninstall-camunda
+
+ifeq ($(OS),Windows_NT)
+    root ?= $(CURDIR)/../../../../camunda-8-helm-profiles
+else
+    root ?= $(shell pwd)/../../../../camunda-8-helm-profiles
+endif
+
+include $(root)/include/camunda.mk
+include $(root)/metrics/metrics.mk # for Grafana URL
