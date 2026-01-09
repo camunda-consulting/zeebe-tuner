8f24b8b2a3fa272b543e3afe07f04d29af2662f0
Falko Menge <falko.menge@camunda.com>
2026-01-09 09:19:54 +0100

    Add Gateway & Partition Pinning

--- src/main/resources/benchmarktemplates/run.sh
+++ src/main/resources/benchmarktemplates/run.sh
@@ -1,2 +1,8 @@
 echo "Waiting $((${loadGeneratorStarter.runDuration}/60)) minutes for completion of benchmark run ${testCaseName} ..."
-sleep ${loadGeneratorStarter.runDuration}
+
+if [[ "$(uname)" != "Darwin" ]] || command -v gtimeout >/dev/null 2>&1; then
+    make logs-benchmark-timed
+else
+    echo "gtimeout is not installed. Using sleep instead. To install gtimeout run: brew install coreutils"
+    sleep ${loadGeneratorStarter.runDuration}
+fi
